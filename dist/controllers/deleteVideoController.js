7481e5a353ea7888eec9b9c3db2dea06f8500e7b
Valentin <valentin_00794@bk.ru>
2024-02-27 21:40:30 +0300

    Ошибка при удалении несуществующего id

--- dist/controllers/deleteVideoController.js
+++ dist/controllers/deleteVideoController.js
@@ -2,12 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteVideoController = void 0;
 const db_1 = require("../db/db");
+const validationErrorsMassages = {
+    id: `Id not found`,
+};
+let apiErrors = [];
 const deleteVideoController = (req, res) => {
     const idVideo = db_1.db.videos.findIndex(video => video.id === +req.params.id);
     if (idVideo === -1) {
+        apiErrors.push({ field: "id", message: validationErrorsMassages.id });
         res
             .status(404)
-            .json({ message: 'id not found' });
+            .json({
+            errorsMessages: apiErrors
+        });
     }
     else {
         db_1.db.videos.splice(idVideo, 1);
--- src/controllers/deleteVideoController.ts
+++ src/controllers/deleteVideoController.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express'
 import { db } from '../db/db'
 import { OutputVideoType } from '../types/video-types'
+import { FieldError } from '../types/validation-types';
+
+const validationErrorsMassages = {
+    id: `Id not found`,
+};
+
+let apiErrors: FieldError[] = [];
 
 export const deleteVideoController = (req: Request, res: Response) => {
     const idVideo = db.videos.findIndex(video => video.id === +req.params.id)
-    if( idVideo === -1) {
+    if (idVideo === -1) {
+        apiErrors.push({ field: "id", message: validationErrorsMassages.id })
         res
-        .status(404)
-        .json({message: 'id not found'})
+            .status(404)
+            .json({
+                errorsMessages: apiErrors
+            })
     } else {
         db.videos.splice(idVideo, 1)
         res
-        .status(204)
+            .status(204)
     }
 }
