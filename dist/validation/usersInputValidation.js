09c1485ec86366224eb8a2b05bca3446e3b534ac
Valentin <valentin_00794@bk.ru>
2024-03-26 16:08:37 +0300

    Валидация при создании пользователя

---
+++ dist/validation/usersInputValidation.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersInputValidation = void 0;
+const express_validator_1 = require("express-validator");
+const VALIDATE_PHARAMS = {
+    password: {
+        minLength: 6,
+        maxLength: 20
+    },
+    login: {
+        minLength: 3,
+        maxLength: 10,
+        pattern: '^[a-zA-Z0-9_-]*$'
+    },
+    email: {
+        pattern: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
+    }
+};
+exports.usersInputValidation = [
+    (0, express_validator_1.body)('password').trim()
+        .notEmpty()
+        .withMessage('The field is required')
+        .isLength({ min: VALIDATE_PHARAMS.password.minLength, max: VALIDATE_PHARAMS.password.maxLength })
+        .withMessage(`The field length should be from ${VALIDATE_PHARAMS.password.minLength} to ${VALIDATE_PHARAMS.password.maxLength}`),
+    (0, express_validator_1.body)('login').trim()
+        .notEmpty()
+        .withMessage('The field is required')
+        .isLength({ min: VALIDATE_PHARAMS.login.minLength, max: VALIDATE_PHARAMS.login.maxLength })
+        .withMessage(`The field length should be from ${VALIDATE_PHARAMS.login.minLength} to ${VALIDATE_PHARAMS.login.maxLength}`)
+        .matches(VALIDATE_PHARAMS.login.pattern).withMessage(`The field has a pattern ${VALIDATE_PHARAMS.login.pattern}`),
+    (0, express_validator_1.body)('email').trim()
+        .notEmpty().withMessage('The field is required')
+        .matches(VALIDATE_PHARAMS.email.pattern).withMessage(`The field has a pattern ${VALIDATE_PHARAMS.email.pattern}`)
+];
--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 import { createUserController, deleteUserController, getUsersController } from "../controllers/usersController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { usersInputValidation } from "../validation/usersInputValidation";
 
 export const usersRouter = Router();
 
 usersRouter.get('/', authMiddleware, getUsersController)
-usersRouter.post('/', authMiddleware, createUserController)
+usersRouter.post('/', authMiddleware, usersInputValidation, createUserController)
 usersRouter.delete('/:id', authMiddleware, deleteUserController)
